2d25f629de568f65c9087ccadf1c8ff47d2daf9e
Bipul <bipul281b@gmail.com>
2025-09-09 21:37:17 +0530

    update: improve mobile table of contents and global styles

--- src/components/ui/MobileTableOfContents.tsx
+++ src/components/ui/MobileTableOfContents.tsx
@@ -184,9 +184,10 @@ const MobileTableOfContents: React.FC = () => {
         className={`mobile-toc-toggle ${isFloating ? 'mobile-toc-toggle-show' : 'mobile-toc-toggle-hide'}`}
         aria-label="Toggle Table of Contents"
       >
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M3 6h18M3 12h18M3 18h18" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 12H5M12 19l-7-7 7-7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
+        <span className="mobile-toc-text">Table of Contents</span>
       </button>
 
       {/* Backdrop */}
